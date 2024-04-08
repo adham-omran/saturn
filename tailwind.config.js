0845e4aef25c5a9bee2edf8a1dcfb6bca942966a
adham-omran <git@adham-omran.com>
2024-04-08 21:50:27 +0300

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./resources/public/index.html',
+              './src/showcase/*.clj',
+              './src/showcase/**/*.clj'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
